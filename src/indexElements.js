20955ab83037591379852546efdc3de367dc7d2f
Yuri <yuriflores2012@gmail.com>
2020-12-03 18:18:00 -0300

    query elements on index html

---
+++ src/indexElements.js
@@ -0,0 +1,17 @@
+const name = document.querySelector('#name');
+const orderValue = document.querySelector('#orderValue');
+const situation = document.querySelector('#situation');
+const orderDate = document.querySelector('#orderDate');
+const requestNumber = document.querySelector('#order');
+const section = document.querySelector('section');
+
+const elements = {
+  name,
+  orderValue,
+  situation,
+  orderDate,
+  requestNumber,
+  section,
+};
+
+export default elements;
