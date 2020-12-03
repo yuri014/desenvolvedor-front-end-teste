ea033f38f50f9861eb9260414169c7d37d96c80c
Yuri <yuriflores2012@gmail.com>
2020-12-03 18:18:47 -0300

    adding functionality to the order request

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
+import elements from './indexElements';
+import { handleError, handleSuccess } from './responseHandlers';
+
 import './style.scss';
+
+const searchOrder = async () => {
+  elements.section.className = 'loading';
+  const fetchOrders = await fetch('./data/dados.json');
+  const response = await fetchOrders.json();
+  const { encomendas } = response;
+  return encomendas.find((order) => order.numero === elements.requestNumber.value.toUpperCase());
+};
+
+const handleOrder = async () => {
+  const order = await searchOrder();
+
+  if (!order) return handleError();
+
+  return handleSuccess(order);
+};
+
+elements.requestNumber.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') handleOrder();
+});
